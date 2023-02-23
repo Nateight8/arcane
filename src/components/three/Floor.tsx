7b4a85b2e0d353c85f3b25e21b731401ef735bae
Mbaocha Jonathan <mbaocha240793@gmail.com>
2023-02-23 14:11:07 +0100

    first one here

---
+++ src/components/three/Floor.tsx
@@ -0,0 +1,47 @@
+import { useTexture, MeshReflectorMaterial } from "@react-three/drei";
+
+import React from "react";
+
+type Props = {};
+
+function Floor({}: Props) {
+  const [normal, rough] = useTexture([
+    "/textures/normal.jpg",
+    "/textures/rough.jpg",
+  ]);
+  return (
+    <>
+      <mesh
+        rotation-x={-Math.PI * 0.5}
+        castShadow
+        receiveShadow
+        position={[3, -1.161, -1.5]}
+      >
+        <planeGeometry args={[30, 30]} />
+        <MeshReflectorMaterial
+          envMapIntensity={0}
+          normalMap={normal}
+          // normalScale={[0.15, 0.15]}
+          roughnessMap={rough}
+          dithering={true}
+          color={[0.015, 0.015, 0.015]}
+          roughness={0.7}
+          blur={[1000, 400]} // Blur ground reflections (width, heigt), 0 skips blur
+          mixBlur={30} // How much blur mixes with surface roughness (default = 1)
+          mixStrength={80} // Strength of the reflections
+          mixContrast={1} // Contrast of the reflections
+          resolution={1024} // Off-buffer resolution, lower=faster, higher=better quality, slower
+          mirror={0} // Mirror environment, 0 = texture colors, 1 = pick up env colors
+          depthScale={0.01} // Scale the depth factor (0 = no depth, default = 0)
+          minDepthThreshold={0.9} // Lower edge for the depthTexture interpolation (default = 0)
+          maxDepthThreshold={1} // Upper edge for the depthTexture interpolation (default = 0)
+          depthToBlurRatioBias={0.25} // Adds a bias factor to the depthTexture before calculating the blur amount [blurFactor = blurTexture * (depthTexture + bias)]. It accepts values between 0 and 1, default is 0.25. An amount > 0 of bias makes sure that the blurTexture is not too sharp because of the multiplication with the depthTexture
+          // debug={0}
+          reflectorOffset={0.2}
+        />
+      </mesh>
+    </>
+  );
+}
+
+export default Floor;
---
+++ src/components/three/SceneLight.tsx
@@ -0,0 +1,99 @@
+import { Environment, Lightformer } from "@react-three/drei";
+import React from "react";
+
+type Props = {};
+
+function SceneLight({}: Props) {
+  return (
+    <>
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <Environment resolution={512}>
+        {/* Ceiling */}
+        <Lightformer
+          intensity={2}
+          rotation-x={Math.PI / 2}
+          position={[0, 4, -9]}
+          scale={[10, 1, 1]}
+        />
+        <Lightformer
+          intensity={2}
+          rotation-x={Math.PI / 2}
+          position={[0, 4, -6]}
+          scale={[10, 1, 1]}
+        />
+        <Lightformer
+          intensity={2}
+          rotation-x={Math.PI / 2}
+          position={[0, 4, -3]}
+          scale={[10, 1, 1]}
+        />
+        <Lightformer
+          intensity={2}
+          rotation-x={Math.PI / 2}
+          position={[0, 4, 0]}
+          scale={[10, 1, 1]}
+        />
+        <Lightformer
+          intensity={2}
+          rotation-x={Math.PI / 2}
+          position={[0, 4, 3]}
+          scale={[10, 1, 1]}
+        />
+        <Lightformer
+          intensity={2}
+          rotation-x={Math.PI / 2}
+          position={[0, 4, 6]}
+          scale={[10, 1, 1]}
+        />
+        <Lightformer
+          intensity={2}
+          rotation-x={Math.PI / 2}
+          position={[0, 4, 9]}
+          scale={[10, 1, 1]}
+        />
+        {/* Sides */}
+        <Lightformer
+          intensity={2}
+          rotation-y={Math.PI / 2}
+          position={[-50, 2, 0]}
+          scale={[100, 2, 1]}
+        />
+        <Lightformer
+          intensity={2}
+          rotation-y={-Math.PI / 2}
+          position={[50, 2, 0]}
+          scale={[100, 2, 1]}
+        />
+        {/* Key */}
+        <Lightformer
+          form="ring"
+          color="red"
+          intensity={10}
+          scale={2}
+          position={[10, 5, 10]}
+          onUpdate={(self) => self.lookAt(0, 0, 0)}
+        />
+      </Environment>
+    </>
+  );
+}
+
+export default SceneLight;
